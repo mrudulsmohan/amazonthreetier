1df1146e81578d35323996f631bac1983de2ab8a
mrudulsmohan <139967133+mrudulsmohan@users.noreply.github.com>
2026-01-05 18:28:42 +0530

    Update App.js

--- application-code/web-tier/src/App.js
+++ application-code/web-tier/src/App.js
@@ -1,47 +1,51 @@
 import React, { useState, useRef } from 'react';
 import { ThemeProvider } from 'styled-components';
-import { useOnClickOutside } from './hooks';
-import { GlobalStyles } from './global';
-import { theme } from './theme';
-import { Burger, Menu } from './components';
 import FocusLock from 'react-focus-lock';
-import DatabaseDemo from './components/DatabaseDemo/DatabaseDemo'
-import Home from './components/Home/Home'
 import {
   HashRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
 
+import { useOnClickOutside } from './hooks';
+import { GlobalStyles } from './global';
+import { theme } from './theme';
+import { Burger, Menu } from './components';
+
+import DatabaseDemo from './components/DatabaseDemo/DatabaseDemo';
+import Home from './components/Home/Home';
 
 function App() {
   const [open, setOpen] = useState(false);
-  const node = useRef();
+  const node = useRef(null);
   const menuId = "main-menu";
 
   useOnClickOutside(node, () => setOpen(false));
 
   return (
     <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <div ref={node}>
-          <FocusLock disabled={!open}>
-            <Burger open={open} setOpen={setOpen} aria-controls={menuId} />
-            <Router>
-              <div>
+      <GlobalStyles />
+
+      <div ref={node}>
+        <FocusLock disabled={!open}>
+          <Burger open={open} setOpen={setOpen} aria-controls={menuId} />
+
+          <Router>
             <Menu open={open} setOpen={setOpen} id={menuId} />
+
             <Switch>
-          <Route path="/db">
-            <DatabaseDemo />
-          </Route>
-          <Route path="/">
-            <Home/>
-          </Route>
-        </Switch>
-        </div>
-        </Router>
-          </FocusLock>
-        </div>
+              <Route path="/db">
+                <DatabaseDemo />
+              </Route>
+
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </Router>
+
+        </FocusLock>
+      </div>
     </ThemeProvider>
   );
 }
