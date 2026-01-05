bf36b740c8414e3af6ff96195dfa859e9d29d0e9
mrudulsmohan <139967133+mrudulsmohan@users.noreply.github.com>
2026-01-05 22:00:27 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
     DB_HOST : 'threetier-db.cfomk8cq25h4.ap-south-1.rds.amazonaws.com',
     DB_USER : 'admin',
-    DB_PWD : 'Manu8547',
+    DB_PWD  : 'Manu8547',
     DB_DATABASE : 'threetier-db'
 });
